30f7b60a0dc23f50030fb70217c3a331984494fe
Phellipe davyson <phellipedavyson@gmail.com>
2022-02-11 17:36:29 -0300

    criando admin

--- controle_estoque/settings.py
+++ controle_estoque/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projeto.core',
+    'core',
+    'produto',
+    'estoque',
 ]
 
 MIDDLEWARE = [
